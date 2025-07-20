6156306255dd1a63c7ae73bb40a292a66e9dc619
Shee-001 <sylviawanjiru804@gmail.com>
2025-07-20 21:33:44 +0300

    deploy successfully

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 // server.js
 const jsonServer = require('json-server');
+const cors = require('cors');
+
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
 const PORT = process.env.PORT || 4000;
 
+server.use(cors());
 server.use(middlewares);
 server.use(router);
+
 server.listen(PORT, () => {
-  console.log(`JSON Server is running on port ${PORT}`);
+  console.log(`✅ JSON Server is running on port ${PORT}`);
 });
