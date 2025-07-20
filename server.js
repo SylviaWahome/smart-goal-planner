b2e59c021fed32a32c590247881f26ec65959c7a
Shee-001 <sylviawanjiru804@gmail.com>
2025-07-20 21:14:09 +0300

    deploy successfully

--- server.js
+++ server.js
@@ -1,24 +1,13 @@
 // server.js
-const jsonServer = require("json-server");
+const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
-server.use(middlewares);
-
-server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
-
-server.use(jsonServer.bodyParser);
-server.use(router);
-
 const PORT = process.env.PORT || 4000;
+
+server.use(middlewares);
+server.use(router);
 server.listen(PORT, () => {
-  console.log(`✅ JSON Server is running on port ${PORT}`);
+  console.log(`JSON Server is running on port ${PORT}`);
 });
